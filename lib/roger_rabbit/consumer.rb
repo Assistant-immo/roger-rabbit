4ba3c1620efe0bea7c3bfdcfaae52bd903f07e2d
Frédéric Grais <frederic@juliedesk.com>
2019-02-19 15:12:00 +0100

    Handling rpc params correctly and yielding to worker block

--- lib/roger_rabbit/consumer.rb
+++ lib/roger_rabbit/consumer.rb
@@ -12,18 +12,22 @@ module RogerRabbit
         queue_config = self.class.get_queue_config(@current_queue.name)
 
         @current_queue.subscribe(block: true, manual_ack: true) do |_delivery_info, _properties, body|
-          success = block.call(body, _properties)
+          max_retry_count = queue_config.fetch(:max_retry_count, 0)
+          headers      = _properties.headers || {}
+          retry_count  = headers.fetch("x-retry-count", 0)
+          correlation_id = extract_correlation_id(_properties)
+          reply_to = extract_reply_to(_properties)
+
+
+          success = block.call(body, _properties, {correlation_id: correlation_id, reply_to: reply_to}, retry_count == max_retry_count)
 
           unless success
             # Taken from https://felipeelias.github.io/rabbitmq/2016/02/22/rabbitmq-exponential-backoff.html
-            headers      = _properties.headers || {}
 
             dead_headers = headers.fetch("x-death", []).last || {}
-            retry_count  = headers.fetch("x-retry-count", 0)
 
             expiration   = dead_headers.fetch("original-expiration", 10000).to_i
 
-            max_retry_count = queue_config.fetch(:max_retry_count, 0)
             retriable = queue_config.fetch(:retriable, false)
             exponential_backoff_factor = queue_config.fetch(:exponential_backoff_factor, 1.1)
 
@@ -34,12 +38,13 @@ module RogerRabbit
               # Publish to retry queue with new expiration
               self.get_retry_queue_for(@current_queue.name).publish(body, expiration: new_expiration.to_i, headers: {
                 "x-retry-count": retry_count + 1,
-                correlation_id: extract_correlation_id(_properties)
+                correlation_id: correlation_id,
+                reply_to: reply_to
               })
             else
               headers = {}
 
-              if correlation_id = extract_correlation_id(_properties)
+              if correlation_id
                 headers.merge!(correlation_id: correlation_id)
               end
 
@@ -60,6 +65,11 @@ module RogerRabbit
       def extract_correlation_id(properties)
         # If retry attempt, correlation id is in the headers hash
         properties[:correlation_id] || properties.headers.fetch('correlation_id', nil)
+      end
+
+      def extract_reply_to(properties)
+        # If retry attempt, correlation id is in the headers hash
+        properties[:reply_to] || properties.headers.fetch('reply_to', nil)
       end
   end
 end
